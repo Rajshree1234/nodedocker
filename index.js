17a721ad55593a2fe76d8e85f618409d0d2f3f23
Rajshree1234 <39979090+Rajshree1234@users.noreply.github.com>
2022-03-07 23:42:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Testing DevOps Node! JS Project Hello How are you ? Rajshree doing good job ')
+  response.send('simple node project ...only for testing purpose')
 }) 
 
 app.listen(app.get('port'), function() {
